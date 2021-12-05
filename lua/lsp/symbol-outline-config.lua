9cfd4a2f5665a667d69b575336e024bc3968cbb6
Xrayya <nur.alkhiyar@gmail.com>
2021-12-06 06:25:17 +0700

    configure outline symbols

---
+++ lua/lsp/symbol-outline-config.lua
@@ -0,0 +1,31 @@
+vim.g.symbols_outline = {
+  width = 45,
+    symbols = {
+      File = {icon = "", hl = "TSURI"},
+      Module = {icon = "", hl = "TSNamespace"},
+      Namespace = {icon = "", hl = "TSNamespace"},
+      Package = {icon = "", hl = "TSNamespace"},
+      Class = {icon = "ﴯ", hl = "TSType"},
+      Method = {icon = "", hl = "TSMethod"},
+      Property = {icon = "ﰠ", hl = "TSMethod"},
+      Field = {icon = "ﰠ", hl = "TSField"},
+      Constructor = {icon = "", hl = "TSConstructor"},
+      Enum = {icon = "", hl = "TSType"},
+      Interface = {icon = "", hl = "TSType"},
+      Function = {icon = "", hl = "TSFunction"},
+      Variable = {icon = "", hl = "TSConstant"},
+      Constant = {icon = "", hl = "TSConstant"},
+      String = {icon = "𝓐", hl = "TSString"},
+      Number = {icon = "1", hl = "TSNumber"},
+      Boolean = {icon = "⊨", hl = "TSBoolean"},
+      Array = {icon = "", hl = "TSConstant"},
+      Object = {icon = "⦿", hl = "TSType"},
+      Key = {icon = "🔐", hl = "TSType"},
+      Null = {icon = "NULL", hl = "TSType"},
+      EnumMember = {icon = "", hl = "TSField"},
+      Struct = {icon = "𝓢", hl = "TSType"},
+      Event = {icon = "", hl = "TSType"},
+      Operator = {icon = "", hl = "TSOperator"},
+      TypeParameter = {icon = "𝙏", hl = "TSParameter"}
+    }
+}
