12459e45a6856fa786bb86b1fa327b30c9870ac0
Xrayya <nur.alkhiyar@gmail.com>
2022-06-24 08:20:56 +0700

    change lsp diagnostic icons

--- lua/lsp-config/handlers.lua
+++ lua/lsp-config/handlers.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- TODO: backfill this to template
 M.setup = function()
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
