9168e33ebd77eee0113acf13b9e94abd53fbcddc
Xrayya <nur.alkhiyar@gmail.com>
2022-06-25 08:12:35 +0700

    add autocommands, funcions, and icons config file

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 require('plugins')
 require('settings')
 require('keymappings')
+require('autocommands')
 
 vim.cmd('source ~/AppData/Local/nvim/vim-config/themes.vim')
 vim.cmd('source ~/AppData/Local/nvim/vim-config/nvide.vim')
