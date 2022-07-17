6fb78fc085321a4e477dffd3cde9c580ea302aec
Xrayya <nur.alkhiyar@gmail.com>
2022-07-17 19:41:43 +0700

    nvim-navic ^& symbols-outline.nvim: fix highlight and refactoring

--- lua/navic-config/init.lua
+++ lua/navic-config/init.lua
@@ -3,70 +3,75 @@ if navic == nil then
   return
 end
 
-local icons = require("icons")
+local _icons = require("icons")
 
 -- stylua: ignore
 navic.setup({
   icons = {
-    File          = " ",
-    Module        = " ",
-    Namespace     = " ",
-    Package       = " ",
-    Class         = " ",
-    Method        = " ",
-    Property      = "ﰠ ",
-    Field         = " ",
-    Constructor   = " ",
-    Enum          = " ",
-    Interface     = " ",
-    Function      = " ",
-    Variable      = " ",
-    Constant      = " ",
-    String        = "𝓐 ",
-    Number        = " ",
-    Boolean       = "﨡 ",
-    Array         = " ",
-    Object        = "⦿ ",
-    Key           = "🔐",
-    Null          = "NULL ",
-    EnumMember    = " ",
-    Struct        = "𝓢 ",
-    Event         = " ",
-    Operator      = " ",
-    TypeParameter = " ",
+    Method        = _icons.kind.Method,
+    Function      = _icons.kind.Function,
+    Constructor   = _icons.kind.Constructor,
+    Field         = _icons.kind.Field,
+    Variable      = _icons.kind.Variable,
+    Class         = _icons.kind.Class,
+    Interface     = _icons.kind.Interface,
+    Module        = _icons.kind.Module,
+    Property      = _icons.kind.Property,
+    Enum          = _icons.kind.Enum,
+    File          = _icons.kind.File,
+    EnumMember    = _icons.kind.EnumMember,
+    Constant      = _icons.kind.Constant,
+    Struct        = _icons.kind.Struct,
+    Event         = _icons.kind.Event,
+    Operator      = _icons.kind.Operator,
+    TypeParameter = _icons.kind.TypeParameter,
+
+    Boolean = _icons.type.Boolean,
+    Number  = _icons.type.Number,
+    String  = _icons.type.String,
+    Array   = _icons.type.Array,
+    Object  = _icons.type.Object,
+
+    Namespace = _icons.other_symbols.Namespace,
+    Package   = _icons.other_symbols.Package,
+    Key       = _icons.other_symbols.Key,
+    Null      = _icons.other_symbols.Null,
   },
 
   highlight = true,
-  separator = " " .. icons.ui.ChevronRight .. " ",
+  separator = " " .. _icons.ui.ChevronRight .. " ",
   depth_limit = 0,
   depth_limit_indicator = "..",
 })
 
-vim.api.nvim_set_hl(0, "NavicIconsFile", { default = true, link = "TSURI" })
-vim.api.nvim_set_hl(0, "NavicIconsModule", { default = true, link = "TSInclude" })
-vim.api.nvim_set_hl(0, "NavicIconsNamespace", { default = true, link = "TSNamespace" })
-vim.api.nvim_set_hl(0, "NavicIconsPackage", { default = true, link = "TSNamespace" })
-vim.api.nvim_set_hl(0, "NavicIconsClass", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsMethod", { default = true, link = "TSMethod" })
-vim.api.nvim_set_hl(0, "NavicIconsProperty", { default = true, link = "TSMethod" })
-vim.api.nvim_set_hl(0, "NavicIconsField", { default = true, link = "TSField" })
-vim.api.nvim_set_hl(0, "NavicIconsConstructor", { default = true, link = "TSConstructor" })
-vim.api.nvim_set_hl(0, "NavicIconsEnum", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsInterface", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsFunction", { default = true, link = "TSFunction" })
-vim.api.nvim_set_hl(0, "NavicIconsVariable", { default = true, link = "TSConstant" })
-vim.api.nvim_set_hl(0, "NavicIconsConstant", { default = true, link = "TSConstant" })
-vim.api.nvim_set_hl(0, "NavicIconsString", { default = true, link = "TSString" })
-vim.api.nvim_set_hl(0, "NavicIconsNumber", { default = true, link = "TSNumber" })
-vim.api.nvim_set_hl(0, "NavicIconsBoolean", { default = true, link = "TSBoolean" })
-vim.api.nvim_set_hl(0, "NavicIconsArray", { default = true, link = "TSConstant" })
-vim.api.nvim_set_hl(0, "NavicIconsObject", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsKey", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsNull", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsEnumMember", { default = true, link = "TSField" })
-vim.api.nvim_set_hl(0, "NavicIconsStruct", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsEvent", { default = true, link = "TSType" })
-vim.api.nvim_set_hl(0, "NavicIconsOperator", { default = true, link = "TSOperator" })
-vim.api.nvim_set_hl(0, "NavicIconsTypeParameter", { default = true, link = "TSParameter" })
+vim.api.nvim_set_hl(0, "NavicIconsMethod", { default = true, link = "CmpItemKindMethod" })
+vim.api.nvim_set_hl(0, "NavicIconsFunction", { default = true, link = "CmpItemKindFunction" })
+vim.api.nvim_set_hl(0, "NavicIconsConstructor", { default = true, link = "CmpItemKindConstructor" })
+vim.api.nvim_set_hl(0, "NavicIconsField", { default = true, link = "CmpItemKindField" })
+vim.api.nvim_set_hl(0, "NavicIconsVariable", { default = true, link = "CmpItemKindVariable" })
+vim.api.nvim_set_hl(0, "NavicIconsClass", { default = true, link = "CmpItemKindClass" })
+vim.api.nvim_set_hl(0, "NavicIconsInterface", { default = true, link = "CmpItemKindInterface" })
+vim.api.nvim_set_hl(0, "NavicIconsModule", { default = true, link = "CmpItemKindModule" })
+vim.api.nvim_set_hl(0, "NavicIconsProperty", { default = true, link = "CmpItemKindProperty" })
+vim.api.nvim_set_hl(0, "NavicIconsEnum", { default = true, link = "CmpItemKindEnum" })
+vim.api.nvim_set_hl(0, "NavicIconsFile", { default = true, link = "CmpItemKindDefault" })
+vim.api.nvim_set_hl(0, "NavicIconsEnumMember", { default = true, link = "CmpItemKindEnumMember" })
+vim.api.nvim_set_hl(0, "NavicIconsConstant", { default = true, link = "CmpItemKindConstant" })
+vim.api.nvim_set_hl(0, "NavicIconsStruct", { default = true, link = "CmpItemKindStruct" })
+vim.api.nvim_set_hl(0, "NavicIconsEvent", { default = true, link = "CmpItemKindEvent" })
+vim.api.nvim_set_hl(0, "NavicIconsOperator", { default = true, link = "CmpItemKindEvent" })
+vim.api.nvim_set_hl(0, "NavicIconsTypeParameter", { default = true, link = "CmpItemKindTypeParameter" })
+
+vim.api.nvim_set_hl(0, "NavicIconsBoolean", { default = true, link = "CmpItemKindValue" })
+vim.api.nvim_set_hl(0, "NavicIconsNumber", { default = true, link = "CmpItemKindValue" })
+vim.api.nvim_set_hl(0, "NavicIconsString", { default = true, link = "CmpItemKindValue" })
+vim.api.nvim_set_hl(0, "NavicIconsArray", { default = true, link = "CmpItemKindVariable" })
+vim.api.nvim_set_hl(0, "NavicIconsObject", { default = true, link = "CmpItemKindValue" })
+
+vim.api.nvim_set_hl(0, "NavicIconsNamespace", { default = true, link = "CmpItemKindModule" })
+vim.api.nvim_set_hl(0, "NavicIconsPackage", { default = true, link = "CmpItemKindModule" })
+vim.api.nvim_set_hl(0, "NavicIconsKey", { default = true, link = "CmpItemKindKeyword" })
+vim.api.nvim_set_hl(0, "NavicIconsNull", { default = true, link = "CmpItemKindValue" })
+
 vim.api.nvim_set_hl(0, "NavicText", { default = true, fg = "#8d8d8d" })
 vim.api.nvim_set_hl(0, "NavicSeparator", { default = true, fg = "#FFFFFF" })
