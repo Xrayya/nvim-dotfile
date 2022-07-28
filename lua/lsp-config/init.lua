bb98dfc499868b04a5594bfe9e98a87227c0d3a6
Xrayya <nur.alkhiyar@gmail.com>
2022-07-28 15:39:27 +0700

    cleaning

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -3,17 +3,6 @@ if lspconfig == nil then
   return
 end
 
-
--- opts = {
---   on_attach = require("lsp-config.handlers").on_attach,
---   capabilities = require("lsp-config.handlers").capabilities,
--- }
-
--- local clangd_opts = require("lsp-config.custom-lsp-settings.sumneko_lua")
--- opts = vim.tbl_deep_extend("force", clangd_opts, opts)
-
--- lspconfig.sumneko_lua.setup(opts)
-
 require("lsp-config.lsp-settings")
 require("lsp-config.handlers").setup()
 require("lsp-config.null-ls")
