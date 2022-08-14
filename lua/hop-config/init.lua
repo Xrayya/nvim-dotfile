ff163244c27245f33b9fbb6e5147efecceb6f82b
Xrayya <nur.alkhiyar@gmail.com>
2022-08-15 05:39:42 +0700

    hop: revert

--- lua/hop-config/init.lua
+++ lua/hop-config/init.lua
@@ -1,13 +1,13 @@
 require("hop").setup()
 
 vim.keymap.set(
-  {"n", "v"},
+  "",
   "f",
   "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR})<cr>",
   {}
 )
 vim.keymap.set(
-  {"n", "v"},
+  "",
   "F",
   "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR})<cr>",
   {}
