1107ba8374a69575a5a58e96d520e11672cc05f8
Xrayya <nur.alkhiyar@gmail.com>
2022-08-18 09:55:21 +0700

    jdtls: save config for future

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -233,6 +233,9 @@ local config = {
           enabled = "all", -- literals, all, none
         },
       },
+      -- inlayHints = {
+      --   includeInlayParameterNamesHints = "all",
+      -- },
       jdt = {
         ls = {
           vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m",
