71ac6ce7e47d9c751ab682f83d0fd8590a079f0e
Xrayya <nur.alkhiyar@gmail.com>
2022-09-05 12:14:30 +0700

    plugins: update `nvim-navic`

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -119,10 +119,11 @@ return require("packer").startup(function(use)
 
     -- GPS / context
     {
-      -- "SmiteshP/nvim-navic",
-      "jason0x43/nvim-navic",
+      "SmiteshP/nvim-navic",
+      -- "jason0x43/nvim-navic",
       -- commit = "94bf6fcb1dc27bdad230d9385da085e72c390019",
-      branch = "symbolinformation-support",
+      -- branch = "symbolinformation-support",
+      branch = "symbolinfo-support",
       requires = "neovim/nvim-lspconfig",
       after = "nvim-lspconfig",
       config = function()
