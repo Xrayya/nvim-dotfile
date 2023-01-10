abd2a3e0a02a8efa713fa1fc194eddeddba79e4d
Xrayya <nur.alkhiyar@gmail.com>
2023-01-10 11:45:15 +0700

    cmp: remove luasnip keymaps for `<Tab>`

--- lua/xrayya/cmp/init.lua
+++ lua/xrayya/cmp/init.lua
@@ -69,12 +69,12 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif luasnip.expandable() then
-        luasnip.expand()
+      -- elseif luasnip.jumpable(1) then
+      --   luasnip.jump(1)
+      -- elseif luasnip.expand_or_jumpable() then
+      --   luasnip.expand_or_jump()
+      -- elseif luasnip.expandable() then
+      --   luasnip.expand()
       elseif has_words_before() then
         -- cmp.complete()
         fallback()
