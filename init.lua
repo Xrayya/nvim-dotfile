31ced873c5b4ce02e9fa72c72197e2c0945093c8
Xrayya <nur.alkhiyar@gmail.com>
2023-04-24 18:39:53 +0700

    add platform check

--- init.lua
+++ init.lua
@@ -5,6 +5,12 @@ require("plugins")
 require("settings")
 require("autocommands")
 
-vim.cmd("source ~/AppData/Local/nvim/vim-config/themes.vim")
-vim.cmd("source ~/AppData/Local/nvim/vim-config/nvide.vim")
-vim.cmd("source ~/AppData/Local/nvim/vim-config/fold-config.vim")
+if vim.fn.has("unix") then
+  vim.cmd("source ~/.config/nvim/vim-config/themes.vim")
+  vim.cmd("source ~/.config/nvim/vim-config/nvide.vim")
+  vim.cmd("source ~/.config/nvim/vim-config/fold-config.vim")
+elseif vim.fn.has("win32") then
+  vim.cmd("source ~/AppData/Local/nvim/vim-config/themes.vim")
+  vim.cmd("source ~/AppData/Local/nvim/vim-config/nvide.vim")
+  vim.cmd("source ~/AppData/Local/nvim/vim-config/fold-config.vim")
+end
