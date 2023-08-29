431957b1b8b14a43df98efde342b883b6d95c0d1
Xrayya <nur.alkhiyar@gmail.com>
2023-08-29 13:48:41 +0700

    plugin: add friendly snippets

--- lua/setup/autocompletion/init.lua
+++ lua/setup/autocompletion/init.lua
@@ -1,12 +1,13 @@
 local autocompletion = {
 	{
 		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
 			local luasnip = require("luasnip")
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
-			luasnip.filetype_extend("javascript", { "javascriptreact" })
+			-- luasnip.filetype_extend("javascript", { "javascriptreact" })
 			luasnip.filetype_extend("php", { "html" })
 
 			local map = vim.keymap.set
