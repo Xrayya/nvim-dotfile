d9c59495f4459ed87f4deb01a4c0b83ddf27922f
Xrayya <nur.alkhiyar@gmail.com>
2023-09-14 23:48:01 +0700

    colorscheme: change LineNr highlight for better control

--- lua/xrayya/setup/colorschemes.lua
+++ lua/xrayya/setup/colorschemes.lua
@@ -1,16 +1,18 @@
 local colorschemes = {
-	{
-		"folke/tokyonight.nvim",
-		config = function()
-			require("tokyonight").setup({
-				style = "moon",
-				light_style = "day",
-				transparent = true,
-			})
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        light_style = "day",
+        transparent = true,
+      })
 
-			vim.cmd.colorscheme("tokyonight-moon")
-		end,
-	},
+      vim.cmd.colorscheme("tokyonight-moon")
+      vim.api.nvim_set_hl(0, "LineNr", { link = "Number" })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { link = "LineNr" })
+    end,
+  },
 }
 
 return colorschemes
