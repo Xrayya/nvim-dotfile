0cc658cedda8cfb83113b66d5796b62ad08eaee9
Xrayya <nur.alkhiyar@gmail.com>
2023-09-14 23:48:37 +0700

    colorizer: switch to NvChad's fork

--- lua/xrayya/setup/colorizer.lua
+++ lua/xrayya/setup/colorizer.lua
@@ -1,13 +1,24 @@
 local colorizer = {
-  "norcalli/nvim-colorizer.lua",
-  config = function()
-    require("colorizer").setup({
-        "*",
-        css = { RRGGBBAA = true, css = true },
-        html = { RRGGBBAA = true, css = true },
-        javascript = { RRGGBBAA = true, css = true },
-    })
-  end,
+	"NvChad/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			filetypes = {
+				"*",
+				css = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				html = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				javascript = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				javascriptreact = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				typescript = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				typescriptreact = { RRGGBBAA = true, css = true, tailwind = "lsp" },
+				cmp_docs = { always_update = true },
+				cmp_menu = { always_update = true },
+			},
+			user_default_options = {
+				RGB = true,
+				RRGGBB = true,
+			},
+		})
+	end,
 }
 
 return colorizer
