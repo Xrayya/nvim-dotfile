5e3c8dd8ab74831b760174afd72e32503ef62c9d
Xrayya <nur.alkhiyar@gmail.com>
2023-11-12 17:31:59 +0700

    clangd: support windows and linux

--- lua/xrayya/setup/lsp/custom-lsp-config/clangd.lua
+++ lua/xrayya/setup/lsp/custom-lsp-config/clangd.lua
@@ -1,10 +1,10 @@
 local query_driver, resource_dir
-if vim.fn.has("win32") then
+if vim.fn.has("win32") > 0 then
   query_driver = "C:\\Users\\Hary\\scoop\\apps\\msys2\\current\\mingw64\\bin\\g++.exe"
   resource_dir = "C:\\Users\\Hary\\scoop\\apps\\msys2\\current\\mingw64\\lib\\gcc\\x86_64-w64-mingw32\\12.2.0\\"
-elseif vim.fn.has("unix") then
+elseif vim.fn.has("unix") > 0 then
   query_driver = "/usr/bin/g++"
-  resource_dir = "/usr/lib/gcc/x86_64-pc-linux-gnu/13.1.1"
+  resource_dir = "/usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1"
 end
 
 return {
