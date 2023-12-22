0a9eb06347835febc51f312844db77b1eeb3a196
Xrayya <nur.alkhiyar@gmail.com>
2023-12-22 17:29:21 +0700

    indent-blankline: upgrade to v3 (ibl)

--- lua/xrayya/setup/indent-blankline.lua
+++ lua/xrayya/setup/indent-blankline.lua
@@ -1,18 +1,41 @@
 local indent_blankline = {
-	"lukas-reineke/indent-blankline.nvim",
-	config = function()
-		require("indent_blankline").setup({
-			buftype_exclude = { "terminal", "nofile" },
-			filetype_exclude = {
-				"alpha",
-				"NvimTree",
-				"help",
-			},
-			space_char_blankline = " ",
-			show_current_context = true,
-			indent_blankline_use_treesitter = true,
-		})
-	end,
+  "lukas-reineke/indent-blankline.nvim",
+  config = function()
+    require("ibl").setup({
+      exclude = {
+        filetypes = {
+          "alpha",
+          "NvimTree",
+        },
+      },
+    })
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
+    local hooks = require("ibl.hooks")
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { link = "RainbowDelimiterRed" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { link = "RainbowDelimiterYellow" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { link = "RainbowDelimiterBlue" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { link = "RainbowDelimiterOrange" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { link = "RainbowDelimiterGreen" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { link = "RainbowDelimiterViolet" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { link = "RainbowDelimiterCyan" })
+    end)
+
+    vim.g.rainbow_delimiters = { highlight = highlight }
+    require("ibl").setup({ scope = { highlight = highlight } })
+
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  end,
 }
 
 return indent_blankline
