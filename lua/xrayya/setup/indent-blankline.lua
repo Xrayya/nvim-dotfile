55785729b711e4fb5634d684f94a20a8739cee0b
Xrayya <nur.alkhiyar@gmail.com>
2023-12-25 17:32:16 +0700

    indent-blankline: fixing wrong setup order

--- lua/xrayya/setup/indent-blankline.lua
+++ lua/xrayya/setup/indent-blankline.lua
@@ -1,26 +1,16 @@
 local indent_blankline = {
   "lukas-reineke/indent-blankline.nvim",
   config = function()
-    require("ibl").setup({
-      exclude = {
-        filetypes = {
-          "alpha",
-          "NvimTree",
-        },
-      },
-    })
     local highlight = {
-      "RainbowRed",
-      "RainbowYellow",
       "RainbowBlue",
       "RainbowOrange",
-      "RainbowGreen",
-      "RainbowViolet",
       "RainbowCyan",
+      "RainbowViolet",
+      "RainbowGreen",
+      "RainbowRed",
+      "RainbowYellow",
     }
     local hooks = require("ibl.hooks")
-    -- create the highlight groups in the highlight setup hook, so they are reset
-    -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "RainbowRed", { link = "RainbowDelimiterRed" })
       vim.api.nvim_set_hl(0, "RainbowYellow", { link = "RainbowDelimiterYellow" })
@@ -32,9 +22,23 @@ local indent_blankline = {
     end)
 
     vim.g.rainbow_delimiters = { highlight = highlight }
-    require("ibl").setup({ scope = { highlight = highlight } })
 
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+    require("ibl").setup({
+      indent = {
+        char = "▏"
+      },
+      exclude = {
+        filetypes = {
+          "alpha",
+          "NvimTree",
+        },
+      },
+      scope = {
+        highlight = highlight
+      }
+    })
   end,
 }
 
