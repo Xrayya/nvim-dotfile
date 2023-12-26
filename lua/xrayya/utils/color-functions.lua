5e831fe319431355ca8f900a93ded03131f211bf
Xrayya <nur.alkhiyar@gmail.com>
2023-12-26 16:43:06 +0700

    utils/color-functions: null safety

--- lua/xrayya/utils/color-functions.lua
+++ lua/xrayya/utils/color-functions.lua
@@ -1,5 +1,8 @@
 local color_functions = {}
 color_functions.convert_decimal_to_hex = function (decimal_value)
+  if decimal_value == nil then
+    return nil
+  end
   return string.format("#%x", decimal_value)
 end
 
