8f62e79660a8f25e6f0cec059f4564df37567696
Xrayya <nur.alkhiyar@gmail.com>
2023-12-26 19:32:40 +0700

    typescript-tools: setup inlayhints

--- lua/xrayya/setup/lsp/advanced-lsp-config/typescript-tools.lua
+++ lua/xrayya/setup/lsp/advanced-lsp-config/typescript-tools.lua
@@ -1,22 +1,22 @@
 local typescript_tools = {
-	"pmizio/typescript-tools.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		local opts = {
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			settings = {
-				publish_diagnostic_on = "change",
-				expose_as_code_action = "all",
-				complete_function_calls = true,
-			},
-		}
-		vim.tbl_deep_extend("force", opts, require("xrayya.setup.lsp.custom-lsp-config.tsserver"))
-
-		require("typescript-tools").setup(opts)
-	end,
+  "pmizio/typescript-tools.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("typescript-tools").setup({
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      settings = {
+        publish_diagnostic_on = "change",
+        expose_as_code_action = "all",
+        complete_function_calls = true,
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+        },
+      },
+    })
+  end,
 }
 
 return typescript_tools
