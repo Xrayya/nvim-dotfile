bb4e713a3e0165c708de41bb26f62b6d6988ec9e
Xrayya <nur.alkhiyar@gmail.com>
2023-12-29 08:13:13 +0700

    global-function: add GET_KEYS and GET_VALUES

--- lua/xrayya/utils/global-functions.lua
+++ lua/xrayya/utils/global-functions.lua
@@ -5,3 +5,19 @@ end
 function LOAD_UTIL(util)
   return require("xrayya.utils." .. util)
 end
+
+function GET_KEYS(table)
+  local keyset = {}
+  for k, _ in pairs(table) do
+    keyset[#keyset + 1] = k
+  end
+  return keyset
+end
+
+function GET_VALUES(table)
+  local valueset = {}
+  for _, v in pairs(table) do
+    valueset[#valueset + 1] = v
+  end
+  return valueset
+end
