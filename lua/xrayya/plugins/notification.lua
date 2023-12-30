bf6a56f2a87973170290b18f32bd0b04210c9e42
Xrayya <nur.alkhiyar@gmail.com>
2023-12-30 20:23:16 +0700

    notification: fix error when hl NotifyBackground not set

--- lua/xrayya/plugins/notification.lua
+++ lua/xrayya/plugins/notification.lua
@@ -2,12 +2,17 @@ local notification = {
   "rcarriga/nvim-notify",
   config = function()
     local notify = require("notify")
+
     vim.notify = notify
-    notify.setup({
+
+    local opts = {
       timeout = 1000,
-      -- background_colour = "#000000",
       fps = 30,
-    })
+    }
+    if vim.g.has_no_bg_highlight then
+      opts.background_colour = "#000000"
+    end
+    notify.setup(opts)
   end,
 }
 
