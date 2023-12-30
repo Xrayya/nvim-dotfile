80780215d9dd96700e260a6255ccf9ed223e3f85
Xrayya <nur.alkhiyar@gmail.com>
2023-12-30 20:22:52 +0700

    colorscheme: add catppuccin

--- lua/xrayya/plugins/colorschemes.lua
+++ lua/xrayya/plugins/colorschemes.lua
@@ -1,6 +1,8 @@
 local colorschemes = {
   {
     "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
       require("tokyonight").setup({
         style = "moon",
@@ -8,9 +10,30 @@ local colorschemes = {
         transparent = true,
       })
 
-      vim.cmd.colorscheme("tokyonight-moon")
       vim.api.nvim_set_hl(0, "LineNr", { link = "Number" })
       vim.api.nvim_set_hl(0, "CursorLineNr", { link = "LineNr" })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.has_no_bg_highlight = true
+    end,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              LineNr = { fg = require("catppuccin.groups.editor").get().CursorLineNr.fg },
+            }
+          end,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
