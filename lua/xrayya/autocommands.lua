29ec4aa5bc2ac33c8979afb31b193bb1ffb28c45
Xrayya <nur.alkhiyar@gmail.com>
2023-12-31 08:36:30 +0700

    create autocmd file

---
+++ lua/xrayya/autocommands.lua
@@ -0,0 +1,9 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+augroup("4Tab", { clear = true })
+autocmd("FileType", {
+  group = "4Tab",
+  pattern = { "c", "cpp", "java", "php" },
+  command = "setlocal tabstop=4 shiftwidth=4 softtabstop=4",
+})
