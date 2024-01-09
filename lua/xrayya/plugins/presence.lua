01f48f442ae577ccfea44eae3e3cc07b5e63ecb1
Xrayya <nur.alkhiyar@gmail.com>
2024-01-10 06:28:15 +0700

    presence: add coc-discord-rpc

--- lua/xrayya/plugins/presence.lua
+++ lua/xrayya/plugins/presence.lua
@@ -1,12 +1,17 @@
 local presence = {
-	"andweeb/presence.nvim",
-	config = function()
-		require("presence").setup({
-			main_image = "file",
-			neovim_text_image = "Insane Text Editor",
-			show_time = true,
-		})
-	end,
+  {
+    "IogaMaster/neocord",
+    enabled = false,
+    event = "VeryLazy",
+    config = function()
+      require("neocord").setup()
+    end,
+  },
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    build = ":CocInstall coc-discord-rpc",
+  },
 }
 
 return presence
