b2b75e5a42ed1ce915d42b5b4b6e9933f7ef8c6b
Xrayya <nur.alkhiyar@gmail.com>
2024-01-10 15:46:10 +0700

    presence: add project detection fallback

--- lua/xrayya/plugins/presence.lua
+++ lua/xrayya/plugins/presence.lua
@@ -1,22 +1,22 @@
 local presence = {
   {
     "Xrayya/neocord",
-    enabled = false,
     event = "VeryLazy",
     config = function()
       require("neocord").setup({
-        global_time = true,
-        -- project_fallback = function(project_path)
-        --   if package.loaded["project_nvim"] then
-        --     local pwd = vim.fn.getcwd()
-        --     return pwd:match(string.format("^.+%s(.+)$", package.config:sub(1, 1))), pwd
-        --   end
-        -- end,
+        global_timer = true,
+        project_fallback = function(project_path)
+          if package.loaded["project_nvim"] and #vim.lsp.buf.list_workspace_folders() > 0 then
+            local pwd = vim.fn.getcwd()
+            return pwd:match(string.format("^.+%s(.+)$", package.config:sub(1, 1))), pwd
+          end
+        end,
       })
     end,
   },
   {
     "neoclide/coc.nvim",
+    enabled = false,
     branch = "release",
     build = ":CocInstall coc-discord-rpc",
   },
