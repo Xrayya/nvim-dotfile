485736e1024037538f9c8cd33f1f3c6a28ae8555
Xrayya <nur.alkhiyar@gmail.com>
2024-01-11 12:17:08 +0700

    presence: add fallback detection

--- lua/xrayya/plugins/presence.lua
+++ lua/xrayya/plugins/presence.lua
@@ -1,11 +1,12 @@
 local presence = {
   {
     "Xrayya/neocord",
+    branch = "project_detection_fallback_function",
     event = "VeryLazy",
     config = function()
       require("neocord").setup({
         global_timer = true,
-        project_fallback = function(project_path)
+        project_detection_fallback_fn = function(file_path)
           if package.loaded["project_nvim"] and #vim.lsp.buf.list_workspace_folders() > 0 then
             local pwd = vim.fn.getcwd()
             return pwd:match(string.format("^.+%s(.+)$", package.config:sub(1, 1))), pwd
@@ -13,12 +14,6 @@ local presence = {
         end,
       })
     end,
-  },
-  {
-    "neoclide/coc.nvim",
-    enabled = false,
-    branch = "release",
-    build = ":CocInstall coc-discord-rpc",
   },
 }
 
