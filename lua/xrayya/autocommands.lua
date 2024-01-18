aa8799bd33958a2d4fae0c4eafde2ba4de9dc0f4
Xrayya <nur.alkhiyar@gmail.com>
2024-01-18 10:40:40 +0700

    autocmd: winbar

--- lua/xrayya/autocommands.lua
+++ lua/xrayya/autocommands.lua
@@ -7,3 +7,12 @@ autocmd("FileType", {
   pattern = { "c", "cpp", "java", "php" },
   command = "setlocal tabstop=4 shiftwidth=4 softtabstop=4",
 })
+
+augroup("winbar", { clear = true })
+autocmd("BufWinEnter", {
+  group = "winbar",
+  pattern = { "[^NvimTree*]" },
+  callback = function(args)
+    vim.opt_local.winbar = (vim.fn.expand("%:t") or vim.bo.filetype)
+  end,
+})
