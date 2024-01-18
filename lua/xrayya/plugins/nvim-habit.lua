13dda8f5fe06c770aa82c938811a27322e1eeb24
Xrayya <nur.alkhiyar@gmail.com>
2024-01-18 10:40:10 +0700

    habit: move vim-be-good to games

--- lua/xrayya/plugins/nvim-habit.lua
+++ lua/xrayya/plugins/nvim-habit.lua
@@ -1,15 +1,16 @@
 local vim_habit = {
   {
-    "theprimeagen/vim-be-good",
-  },
-  {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     config = function()
+      local disabled_filetypes = require("hardtime.config").config.disabled_filetypes
+      table.insert(disabled_filetypes, "sudoku")
+
       require("hardtime").setup({
         max_time = 2000,
         max_count = 8,
         disable_mouse = false,
+        disabled_filetypes = disabled_filetypes,
         allow_different_key = true,
       })
     end,
