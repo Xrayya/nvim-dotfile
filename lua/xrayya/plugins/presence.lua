c7a75f84d37940fbbc011a2bba6b20ad25705c75
Xrayya <nur.alkhiyar@gmail.com>
2024-01-25 15:45:54 +0700

    presence: change project detection fallback and workspace text

--- lua/xrayya/plugins/presence.lua
+++ lua/xrayya/plugins/presence.lua
@@ -7,9 +7,16 @@ local presence = {
       require("neocord").setup({
         global_timer = true,
         project_detection_fallback_fn = function(file_path)
-          if package.loaded["project_nvim"] and #vim.lsp.buf.list_workspace_folders() > 0 then
+          if package.loaded["project_nvim"] then
             local pwd = vim.fn.getcwd()
             return pwd:match(string.format("^.+%s(.+)$", package.config:sub(1, 1))), pwd
+          end
+        end,
+        workspace_text = function(project_name, file_name)
+          if file_name ~= "" and #vim.lsp.buf.list_workspace_folders() > 0 then
+            return string.format("Project: %s", project_name)
+          else
+            return string.format("Workspace: %s", project_name)
           end
         end,
       })
