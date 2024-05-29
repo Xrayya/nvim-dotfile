3c6d4aa6be968d1201f40b641b0825c19bee5e68
Xrayya <nur.alkhiyar@gmail.com>
2024-05-29 09:57:43 +0700

    ui: hide incline on cursorline

--- lua/xrayya/plugins/ui.lua
+++ lua/xrayya/plugins/ui.lua
@@ -95,6 +95,9 @@ local ui = {
           padding = 0,
           margin = { horizontal = 0 },
         },
+				hide = {
+					cursorline = true,
+				},
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if filename == "" then
