ae1cf665d158aaf341d3ad3fdb400b75be4f20f6
Xrayya <nur.alkhiyar@gmail.com>
2024-05-30 06:25:08 +0700

    navigation: add vim-tmux-navigator

--- lua/xrayya/plugins/navigation.lua
+++ lua/xrayya/plugins/navigation.lua
@@ -42,6 +42,23 @@ local navigation = {
       })
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
 
 return navigation
