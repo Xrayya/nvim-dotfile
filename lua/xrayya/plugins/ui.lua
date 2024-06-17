f66755007ce240a634cb7bb2d70e84f08b59458f
Xrayya <nur.alkhiyar@gmail.com>
2024-06-18 05:22:00 +0700

    ui: add keymap for lsp doc scroll

--- lua/xrayya/plugins/ui.lua
+++ lua/xrayya/plugins/ui.lua
@@ -32,6 +32,18 @@ local ui = {
           },
         },
       })
+
+      vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+        if not require("noice.lsp").scroll(1) then
+          return "<c-f>"
+        end
+      end, { silent = true, expr = true })
+
+      vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+        if not require("noice.lsp").scroll(-1) then
+          return "<c-b>"
+        end
+      end, { silent = true, expr = true })
     end,
   },
   {
