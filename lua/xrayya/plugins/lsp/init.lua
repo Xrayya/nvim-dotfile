45dad784dc931b322a17fa3d6cfcff113e03f291
Xrayya <nur.alkhiyar@gmail.com>
2024-06-25 05:34:52 +0700

    lsp: add `lazydev.nvim`

--- lua/xrayya/plugins/lsp/init.lua
+++ lua/xrayya/plugins/lsp/init.lua
@@ -117,6 +117,18 @@ local lsp = {
     "mfussenegger/nvim-jdtls",
   },
   require("xrayya.plugins.lsp.advanced-lsp-config"),
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 }
 
 return lsp
