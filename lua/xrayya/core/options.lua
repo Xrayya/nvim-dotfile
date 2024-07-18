cc0886128ec699d9eefcb5eee491c6383821580e
Xrayya <nur.alkhiyar@gmail.com>
2024-07-18 09:55:17 +0700

    options: setup kitty undercurl

--- lua/xrayya/core/options.lua
+++ lua/xrayya/core/options.lua
@@ -39,3 +39,6 @@ set.timeoutlen = 300
 set.clipboard:append({ "unnamedplus" })
 set.shortmess:append("c")
 set.termguicolors = true
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
