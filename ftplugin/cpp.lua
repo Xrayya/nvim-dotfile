5e99614185004d9059a6a904e9b4529b6c932ae2
Xrayya <nur.alkhiyar@gmail.com>
2024-07-29 13:40:19 +0700

    cpp: setup keymap for compiling

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,27 +1,22 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.opt.rtp:prepend(lazypath)
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-vim.cmd([[Lazy load which-key.nvim]])
-require("setup.whichkey")
+map("n", "<F5>", function()
+  local input_file = vim.fn.expand("%:p")
+  local output_file = vim.fn.expand("%:p:r")
 
-require("which-key").register({
-	C = {
-		name = "C++",
-		c = {
-			":!g++ -fdiagnostics-color=always -g "
-				.. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-				.. " -o "
-				.. vim.fn.fnamemodify(vim.fn.getcwd(), ":r")
-				.. "<cr><cmd>lua vim.notify('Done Compiling')<cr>",
-			"Compile",
-		},
-	},
-	{
-		mode = "n",
-		prefix = "<leader>",
-		buffer = nil,
-		silent = true,
-		noremap = true,
-		nowait = true,
-	},
-})
+  if vim.fn.has("win32") > 0 then
+    output_file = output_file .. ".exe"
+  else
+    output_file = output_file .. ".out"
+  end
+
+  local cmd = string.format('g++ -fdiagnostics-color=always -g "%s" -o "%s"', input_file, output_file)
+  local result = vim.fn.system(cmd)
+
+  if string.find(result, "error") then
+    vim.notify(result, vim.log.levels.ERROR)
+  else
+    vim.notify("Compiling done", vim.log.levels.INFO)
+  end
+end, opts)
