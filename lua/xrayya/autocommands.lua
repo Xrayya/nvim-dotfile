432b0bb5489235c5cba8345aa49619498bc14d28
Xrayya <nur.alkhiyar@gmail.com>
2024-09-12 10:30:38 +0700

    autocommands: remove `c` and `cpp` from 4Tab autocmd

--- lua/xrayya/autocommands.lua
+++ lua/xrayya/autocommands.lua
@@ -4,6 +4,6 @@ local augroup = vim.api.nvim_create_augroup
 augroup("4Tab", { clear = true })
 autocmd("FileType", {
   group = "4Tab",
-  pattern = { "c", "cpp", "java", "php" },
+  pattern = { "java", "php" },
   command = "setlocal tabstop=4 shiftwidth=4 softtabstop=4",
 })
