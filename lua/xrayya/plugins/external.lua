1286adad2f39933d016578b2a531a20e7eae7bb6
Xrayya <nur.alkhiyar@gmail.com>
2024-09-25 12:32:12 +0700

    external: add cord

--- lua/xrayya/plugins/external.lua
+++ lua/xrayya/plugins/external.lua
@@ -1,29 +1,22 @@
 local external = {
   {
-    "Xrayya/neocord",
-    enabled = false,
-    branch = "project_detection_fallback_function",
+    "vyfor/cord.nvim",
     event = "VeryLazy",
     config = function()
-      require("neocord").setup({
-        global_timer = true,
-        project_detection_fallback_fn = function(file_path)
-          if package.loaded["project_nvim"] then
-            local pwd = vim.fn.getcwd()
-            return pwd:match(string.format("^.+%s(.+)$", package.config:sub(1, 1))), pwd
-          end
-        end,
-        workspace_text = function(project_name, file_name)
-          if file_name ~= "" and #vim.lsp.buf.list_workspace_folders() > 0 then
-            return string.format("Project: %s", project_name)
-          else
-            return string.format("Workspace: %s", project_name)
-          end
-        end,
+      require("cord").setup({
+        text = {
+          viewing = "Viewing {}",
+          editing = "Editing {}",
+          file_browser = "Browsing files in {}",
+          plugin_manager = "Managing plugins in {}",
+          lsp_manager = "Configuring LSP in {}",
+          vcs = "Committing changes in {}",
+          workspace = "Workspace: {}",
+        },
       })
     end,
   },
-  { 'wakatime/vim-wakatime', lazy = false }
+  { "wakatime/vim-wakatime", lazy = false },
 }
 
 return external
