f455637c2733ba479901bc6a70b5123a981071a9
Xrayya <nur.alkhiyar@gmail.com>
2024-12-25 09:00:33 +0700

    ui: change smear cursor config

--- lua/xrayya/plugins/ui.lua
+++ lua/xrayya/plugins/ui.lua
@@ -65,12 +65,9 @@ local ui = {
   },
   {
     "sphamba/smear-cursor.nvim",
-    opts = {
-      smear_between_buffers = true,
-      smear_between_neighbor_lines = true,
-      scroll_buffer_space = true,
-      legacy_computing_symbols_support = true,
-    },
+    config = function()
+      require("smear_cursor").setup({})
+    end,
   },
   {
     "HiPhish/rainbow-delimiters.nvim",
