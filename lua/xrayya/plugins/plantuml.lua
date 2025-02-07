a1aa31811c701813f704e33af2b661804f657b46
Xrayya <nur.alkhiyar@gmail.com>
2025-02-07 07:10:57 +0700

    plantuml: make the usercommand run async

--- lua/xrayya/plugins/plantuml.lua
+++ lua/xrayya/plugins/plantuml.lua
@@ -1,24 +1,34 @@
-vim.api.nvim_create_user_command("PlanumlCompile", function(args)
-  local input_file = vim.fn.expand("%:p")
-  local output_file = vim.fn.expand("%:p:r") .. ".png"
+vim.api.nvim_create_user_command(
+  "PlanumlCompile",
+  function()
+    local input_file = vim.fn.expand("%:p")
+    local output_file = vim.fn.expand("%:p:r") .. ".png"
 
-  local cmd = string.format('plantuml "%s"', input_file)
-  local result = vim.fn.system(cmd)
-
-  if string.find(result, "Error") or string.find(result, "Warning") then
-    vim.notify(result, vim.log.levels.ERROR, {
-      title = "PlantUML Compilation",
-    })
-  else
-    vim.notify("Compiling done", vim.log.levels.INFO, {
-      title = "PlantUML Compilation",
-    })
-    vim.notify("Output file: " .. output_file, vim.log.levels.INFO, {
-      title = "PlantUML Compilation",
-    })
-    vim.fn.system(string.format('xdg-open "%s"', output_file))
-  end
-end, {})
+    vim.uv.spawn(
+      "plantuml",
+      ---@diagnostic disable-next-line: missing-fields
+      {
+        args = { input_file },
+      },
+      vim.schedule_wrap(function(code, _)
+        if code == 0 then
+          vim.notify("Compiling done", vim.log.levels.INFO, {
+            title = "PlantUML Compilation",
+          })
+          vim.notify("Output file: " .. output_file, vim.log.levels.INFO, {
+            title = "PlantUML Compilation",
+          })
+          vim.fn.system(string.format('xdg-open "%s"', output_file))
+        else
+          vim.notify("Compiling failed", vim.log.levels.ERROR, {
+            title = "PlantUML Compilation",
+          })
+        end
+      end)
+    )
+  end,
+  {}
+)
 
 local plantuml = {
   "javiorfo/nvim-soil",
