64b2b2ab52ca39b8b6814d838d09e156d40991b0
Xrayya <nur.alkhiyar@gmail.com>
2025-02-17 09:14:25 +0700

    commentary: use native neovim commentary

--- lua/xrayya/plugins/commentary.lua
+++ lua/xrayya/plugins/commentary.lua
@@ -1,36 +1,17 @@
 local commentary = {
   {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
+    "JoosepAlviste/nvim-ts-context-commentstring",
     config = function()
-      require("Comment").setup({
-        ignore = "^$",
-        pre_hook = function(ctx)
-          require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-
-          if
-            vim.bo.filetype == "typescriptreact"
-            or vim.bo.filetype == "javascript"
-            or vim.bo.filetype == "javascriptreact"
-          then
-            local U = require("Comment.utils")
-
-            local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-            local location = nil
-            if ctx.ctype == U.ctype.blockwise then
-              location = require("ts_context_commentstring.utils").get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-              location = require("ts_context_commentstring.utils").get_visual_start_location()
-            end
-
-            return require("ts_context_commentstring.internal").calculate_commentstring({
-              key = type,
-              location = location,
-            })
-          end
-        end,
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
       })
+
+      local get_option = vim.filetype.get_option
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.filetype.get_option = function(filetype, option)
+        return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+          or get_option(filetype, option)
+      end
     end,
   },
 }
