92c636d5b465fdc3c61bcdf057936a14e0361fe2
Xrayya <nur.alkhiyar@gmail.com>
2025-03-28 13:26:11 +0700

    commentary: add `todo-comments.nvim`

--- lua/xrayya/plugins/commentary.lua
+++ lua/xrayya/plugins/commentary.lua
@@ -14,6 +14,11 @@ local commentary = {
       end
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
 }
 
 return commentary
